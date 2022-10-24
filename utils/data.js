5ace03f309367f150436f0a7e5418baed6bffa86
rlaalsdn <kmw011122@gmail.com>
2022-10-24 12:25:57 +0900

    10-ConnectToMongoDB

--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,21 @@
+import bcrypt from 'bcryptjs'
+
 const data = {
+  users: [
+    {
+      name: 'john',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'jane',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+
   products: [
     {
       name: 'Free Shirt',
